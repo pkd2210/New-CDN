97967be2544da120064ea93d787f173e540c30e6
pelegozcellact <pelegoz@cellact.com>
2026-04-07 22:56:06 +0300

    Add bucket delete function

---
+++ src/routes/api/buckets/delete/[bucket]/+server.js
@@ -0,0 +1,28 @@
+import { auth } from '$lib/server/auth'; // Better-auth instance
+import { db } from '$lib/server/db'; // Drizzle ORM instance
+import { files, buckets } from '$lib/server/db/schema'; // Drizzle ORM schema for files and buckets
+import { eq } from 'drizzle-orm'; // Drizzle ORM helper for equality checks
+
+export async function GET({ params, request }) {
+    const { bucket } = params;
+
+    // Check if user is authenticated thorugh better auth
+    const session = await auth.api.getSession({
+        headers: request.headers
+    });
+    if (!session?.user) {
+        return new Response('Unauthorized', { status: 401 });
+    }
+    // Check if user is the owner of the bucket
+    const [bucketExists] = await db.select().from(buckets).where(eq(buckets.name, bucket)).limit(1);
+    if (!bucketExists) {
+        return new Response('Bucket not found', { status: 404 });
+    }
+    if (bucketExists.userId !== session.user.id) {
+        return new Response('Access denied', { status: 403 });
+    }
+    // Delete the bucket
+    await db.delete(buckets).where(eq(buckets.name, bucket));
+
+    return new Response('Bucket deleted successfully', { status: 200 });
+}
